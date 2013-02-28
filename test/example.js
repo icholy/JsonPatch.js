c67ecfabe61c416f7845668c3e60337e7858a1f7
Ilia Choly <icholy@accipiterradar.com>
2013-02-28 16:08:16 -0500

    better slice handling

--- test/example.js
+++ test/example.js
@@ -1,132 +1,132 @@
-// var assert = require('chai').assert,
-//     JsonPatch = require('../lib');
+var assert = require('chai').assert,
+    JsonPatch = require('../lib');
 
-// describe('JsonPatch', function () {
+describe('JsonPatch', function () {
 
-//   describe('#apply', function () {
+  describe('#apply', function () {
 
-//     var patcher, data;
+    var patcher, data;
 
-//     beforeEach(function () {
-//       patcher = new JsonPatch();
-//       data = {
-//         who: { what: { where: "here" } },
-//         bar: [1, 2, 3, 4, 5],
-//         baz: [
-//           { x: true,  y: "a" },
-//           { x: false, y: "b" },
-//           { x: true,  y: "c" },
-//           { x: true,  y: "d" },
-//           { x: false, y: "e" }
-//         ]
-//       };
-//     });
+    beforeEach(function () {
+      patcher = new JsonPatch();
+      data = {
+        who: { what: { where: "here" } },
+        bar: [1, 2, 3, 4, 5],
+        baz: [
+          { x: true,  y: "a" },
+          { x: false, y: "b" },
+          { x: true,  y: "c" },
+          { x: true,  y: "d" },
+          { x: false, y: "e" }
+        ]
+      };
+    });
 
-//     describe('add', function () {
-//       it('should add basic property', function () {
-//         patcher.apply(data, [{ op: 'add', path: '/boo', value: "this is a test" }]);
-//         assert.property(data, 'boo');
-//         assert.equal(data.boo, "this is a test");
-//       });
+    describe('add', function () {
+      it('should add basic property', function () {
+        patcher.apply(data, [{ op: 'add', path: '/boo', value: "this is a test" }]);
+        assert.property(data, 'boo');
+        assert.equal(data.boo, "this is a test");
+      });
 
-//       it('should add arrays', function () {
-//         patcher.apply(data, [{ op: 'add', path: '/boo', value: [1, 2, 3] }]);
-//         assert.property(data, 'boo');
-//         assert.deepEqual(data.boo, [1, 2, 3]);
-//       });
+      it('should add arrays', function () {
+        patcher.apply(data, [{ op: 'add', path: '/boo', value: [1, 2, 3] }]);
+        assert.property(data, 'boo');
+        assert.deepEqual(data.boo, [1, 2, 3]);
+      });
 
-//       it('should add properties to slices', function () {
-//         patcher.apply(data, [{ op: 'add', path: '/baz/0:5/z', value: "z-value" }]);
-//         data.baz.forEach(function (obj) { assert.equal(obj.z, "z-value"); });
-//       });
-//     });
+      it('should add properties to slices', function () {
+        patcher.apply(data, [{ op: 'add', path: '/baz/0:5/z', value: "z-value" }]);
+        data.baz.forEach(function (obj) { assert.equal(obj.z, "z-value"); });
+      });
+    });
 
-//     describe('replace', function () {
+    describe('replace', function () {
 
-//       it('should replace basic properties', function () {
-//         patcher.apply(data, [{ op: 'replace', path: '/who/what/where', value: "new_value" }]);
-//         assert.equal(data.who.what.where, "new_value");
-//       });
+      it('should replace basic properties', function () {
+        patcher.apply(data, [{ op: 'replace', path: '/who/what/where', value: "new_value" }]);
+        assert.equal(data.who.what.where, "new_value");
+      });
 
-//       it('should replace array indexes', function () {
-//         patcher.apply(data, [{ op: 'replace', path: '/baz/0', value: "this is new" }]);
-//         assert.equal(data.baz[0], "this is new");
-//       });
+      it('should replace array indexes', function () {
+        patcher.apply(data, [{ op: 'replace', path: '/baz/0', value: "this is new" }]);
+        assert.equal(data.baz[0], "this is new");
+      });
 
-//       it("should replace properties in slices", function () {
-//         patcher.apply(data, [{ op: 'replace', path: '/baz/0:2/x', value: "hello" }]);
-//         assert.equal(data.baz[0].x, "hello");
-//         assert.equal(data.baz[1].x, "hello");
-//       });
+      it("should replace properties in slices", function () {
+        patcher.apply(data, [{ op: 'replace', path: '/baz/0:2/x', value: "hello" }]);
+        assert.equal(data.baz[0].x, "hello");
+        assert.equal(data.baz[1].x, "hello");
+      });
 
-//       it('should replace slices with a single value', function () {
-//         patcher.apply(data, [{ op: 'replace', path: '/bar/0:3', value: 123 }]);
-//         assert.lengthOf(data.bar, 5);
-//         assert.deepEqual(data.bar, [123, 123, 123, 4, 5]);
-//       });
+      it('should replace slices with a single value', function () {
+        patcher.apply(data, [{ op: 'replace', path: '/bar/0:3', value: 123 }]);
+        assert.lengthOf(data.bar, 3);
+        assert.deepEqual(data.bar, [123, 4, 5]);
+      });
 
-//       it('should replace slices with arrays', function () {
-//         patcher.apply(data, [{ op: 'replace', path: '/bar/1:3', value: ["A", "B", "C"] }]);
-//         assert.lengthOf(data.bar, 6);
-//         assert.deepEqual(data.bar, [1, 'A', 'B', 'C', 4, 5]);
-//       });
-//     });
+      it('should replace slices with arrays', function () {
+        patcher.apply(data, [{ op: 'replace', path: '/bar/1:3', value: ["A", "B", "C"] }]);
+        assert.lengthOf(data.bar, 6);
+        assert.deepEqual(data.bar, [1, 'A', 'B', 'C', 4, 5]);
+      });
+    });
 
-//     describe('remove', function () {
+    describe('remove', function () {
 
-//       it('should remove a property', function () {
-//         patcher.apply(data, [{ op: 'remove', path: '/who' }]);
-//         assert.isUndefined(data.who);
-//       });
+      it('should remove a property', function () {
+        patcher.apply(data, [{ op: 'remove', path: '/who' }]);
+        assert.isUndefined(data.who);
+      });
 
-//       it('should remove deeper properties', function () {
-//         patcher.apply(data, [{ op: 'remove', path: '/who/what/where' }]);
-//         assert.isUndefined(data.who.what.where);
-//       });
+      it('should remove deeper properties', function () {
+        patcher.apply(data, [{ op: 'remove', path: '/who/what/where' }]);
+        assert.isUndefined(data.who.what.where);
+      });
 
-//       it('should remove array indexes', function () {
-//         patcher.apply(data, [{ op: 'remove', path: '/baz/0' }]);
-//         assert.lengthOf(data.baz, 4);
-//         assert.deepEqual(data.baz[0], { x: false, y: "b" });
-//       });
+      it('should remove array indexes', function () {
+        patcher.apply(data, [{ op: 'remove', path: '/baz/0' }]);
+        assert.lengthOf(data.baz, 4);
+        assert.deepEqual(data.baz[0], { x: false, y: "b" });
+      });
 
-//       it('should remove properties in slices', function () {
-//         patcher.apply(data, [{ op: 'remove', path: '/baz/1:3/x' }]);
-//         assert.isUndefined(data.baz[1].x);
-//         assert.isUndefined(data.baz[2].x);
-//       });
+      it('should remove properties in slices', function () {
+        patcher.apply(data, [{ op: 'remove', path: '/baz/1:3/x' }]);
+        assert.isUndefined(data.baz[1].x);
+        assert.isUndefined(data.baz[2].x);
+      });
 
-//       it('should remove slices', function () {
-//         patcher.apply(data, [{ op: 'remove', path: '/baz/2:4' }]);
-//         assert.lengthOf(data.baz, 3);
-//       });
-//     });
+      it('should remove slices', function () {
+        patcher.apply(data, [{ op: 'remove', path: '/baz/2:4' }]);
+        assert.lengthOf(data.baz, 3);
+      });
+    });
 
-//     describe('move', function () {
+    describe('move', function () {
 
-//       it('should move basic properties', function () {
-//         patcher.apply(data, [{ op: 'move', from: '/who/what/where', to: '/who/what/when' }]);
-//         assert.equal(data.who.what.when, "here");
-//         assert.isUndefined(data.who.what.where);
-//       });
+      it('should move basic properties', function () {
+        patcher.apply(data, [{ op: 'move', from: '/who/what/where', to: '/who/what/when' }]);
+        assert.equal(data.who.what.when, "here");
+        assert.isUndefined(data.who.what.where);
+      });
 
-//       it('should move objects', function () {
-//         patcher.apply(data, [{ op: 'move', from: '/who', to: '/poo' }]);
-//         assert.isUndefined(data.who);
-//         assert.equal(data.poo.what.where, "here");
-//       });
+      it('should move objects', function () {
+        patcher.apply(data, [{ op: 'move', from: '/who', to: '/poo' }]);
+        assert.isUndefined(data.who);
+        assert.equal(data.poo.what.where, "here");
+      });
 
-//       it('should move slices', function () {
-//         //patcher.apply(data, [{ op: 'move', from: '/bar/0:2', to: '/poo' }]);
-//         // assert.lengthOf(data.bar, 3);
-//         // assert.lengthOf(data.poo, 2);
-//         // assert.deepEqual(data.poo, [1, 2]);
-//       });
-//     });
+      it('should move slices', function () {
+        //patcher.apply(data, [{ op: 'move', from: '/bar/0:2', to: '/poo' }]);
+        // assert.lengthOf(data.bar, 3);
+        // assert.lengthOf(data.poo, 2);
+        // assert.deepEqual(data.poo, [1, 2]);
+      });
+    });
 
-//     describe('copy', function () {
+    describe('copy', function () {
 
-//     });
+    });
 
-//   })
-// });
+  })
+});
--- test/test.js
+++ test/test.js
@@ -105,7 +105,7 @@ describe('JsonPatch', function () {
 			assert.lengthOf(A.bar, 2);
 		});
 
-		it.only('should remove properties in each of the slice elements', function () {
+		it('should remove properties in each of the slice elements', function () {
 			var result = c('/baz/0:1/x');
 			assert.property(A.baz[0], 'x');
 			patcher.remove(result);
@@ -115,77 +115,138 @@ describe('JsonPatch', function () {
 	});
 
 	describe('#replace', function () {
-		// it('should replace the property value', function () {
-		// 	var result = c('/foo');
-		// 	assert.equal(A.foo, "hello world");
-		// 	patcher.replace(result, "new value");
-		// 	assert.equal(A.foo, "new value");
-		// });
+		it('should replace the property value', function () {
+			var result = c('/foo');
+			assert.equal(A.foo, "hello world");
+			patcher.replace(result, "new value");
+			assert.equal(A.foo, "new value");
+		});
 
-		// it('should replace an array index value', function () {
-		// 	var result = c('/bar/1');
-		// 	assert.equal(A.bar[1], 2);
-		// 	patcher.replace(result, 100);
-		// 	assert.equal(A.bar[1], 100);
-		// });
+		it('should replace an array index value', function () {
+			var result = c('/bar/1');
+			assert.equal(A.bar[1], 2);
+			patcher.replace(result, 100);
+			assert.equal(A.bar[1], 100);
+		});
 
-		// it('should replace the slice with a single value', function () {
-		// 	var result = c('/bar/0:2');
-		// 	assert.deepEqual(A.bar, [1, 2, 3, 4]);
-		// 	patcher.replace(result, ["hello"]);
-		// 	assert.deepEqual(A.bar, ["hello", 3, 4]);
-		// });
+		it('should replace the slice with a single value', function () {
+			var result = c('/bar/0:2');
+			assert.deepEqual(A.bar, [1, 2, 3, 4]);
+			patcher.replace(result, ["hello"]);
+			assert.deepEqual(A.bar, ["hello", 3, 4]);
+		});
 
-		// it('should replce splice element contents', function () {
-		// 	var result = c('/baz/1:3/x');
-		// 	patcher.replace(result, "what");
-		// 	assert.equal(A.baz[1].x, "what");
-		// 	assert.equal(A.baz[2].x, "what");
-		// });
+		it('should replce splice element contents', function () {
+			var result = c('/baz/1:3/x');
+			patcher.replace(result, "what");
+			assert.equal(A.baz[1].x, "what");
+			assert.equal(A.baz[2].x, "what");
+		});
+
+		it('should replace splice with 1 array element if it is [[]]', function () {
+			var result = c('/baz/0:2');
+			patcher.replace(result, [["a", "b", "c"]]);
+			assert.lengthOf(A.baz, 3);
+			assert.deepEqual(A.baz[0], ["a", "b", "c"]);
+		});
 	});
 
 	describe('#add', function () {
-		// it('should add a value on a new key', function () {
-		// 	var result = c('/not_there');
-		// 	assert.isUndefined(A.not_there);
-		// 	patcher.add(result, "hi");
-		// 	assert.equal(A.not_there, "hi");
-		// });
+		it('should add a value on a new key', function () {
+			var result = c('/not_there');
+			assert.isUndefined(A.not_there);
+			patcher.add(result, "hi");
+			assert.equal(A.not_there, "hi");
+		});
+
+		it('should add a value at an array index and shift the other values', function () {
+			var result = c('/bar/0');
+			assert.lengthOf(A.bar, 4);
+			patcher.add(result, -1);
+			assert.lengthOf(A.bar, 5);
+			assert.equal(A.bar[0], -1);
+		});
 	});
 
 	describe('#move', function () {
-		// it('should move a basic property', function () {
-		// 	var from = c('/foo'),
-		// 			to = c('/foo2');
-		// 	assert.isUndefined(A.foo2);
-		// 	assert.property(A, 'foo');
-		// 	patcher.move(from, to);
-		// 	assert.property(A, 'foo2');
-		// 	assert.isUndefined(A.foo);
-		// });
+		it('should move a basic property', function () {
+			var from = c('/foo'),
+					to = c('/foo2');
+			assert.isUndefined(A.foo2);
+			assert.property(A, 'foo');
+			patcher.move(from, to);
+			assert.property(A, 'foo2');
+			assert.isUndefined(A.foo);
+		});
 
-		// it('should move a slice', function () {
-		// 	var from = c('/bar/0:3'),
-		// 			to = c('/boo');
-		// 	console.log(from);
-		// 	assert.isUndefined(A.boo);
-		// 	assert.lengthOf(A.bar, 4);
-		// 	patcher.move(from, to);
-		// 	assert.property(A, 'boo');
-		// 	assert.lengthOf(A.boo, 3);
-		// 	assert.lengthOf(A.bar, 1);
-		// });
+		it('should move a slice', function () {
+			var from = c('/bar/0:3'),
+					to = c('/boo');
+			assert.isUndefined(A.boo);
+			assert.lengthOf(A.bar, 4);
+			patcher.move(from, to);
+			assert.property(A, 'boo');
+			assert.lengthOf(A.boo, 3);
+			assert.lengthOf(A.bar, 1);
+		});
 
-		// it('should move properties inside slices', function () {
-		// 	var from = c('/baz/0:4/x'),
-		// 		to = c('/xs');
-		// 	assert.isUndefined(A.xs);
-		// 	patcher.move(from, to);
-		// 	assert.property(A, 'xs');
-		// 	assert.typeOf(A.xs, "array");
-		// 	assert.lengthOf(A.xs, 4);
-		// 	assert.deepEqual(A.xs, ["a", "b", "c", "d"]);
-		// 	assert.isUndefined(A.baz[0].x);
-		// });
+		it('should move properties inside slices', function () {
+			var from = c('/baz/0:4/x'),
+				to = c('/xs');
+			assert.isUndefined(A.xs);
+			patcher.move(from, to);
+			assert.property(A, 'xs');
+			assert.typeOf(A.xs, "array");
+			assert.lengthOf(A.xs, 4);
+			assert.deepEqual(A.xs, ["a", "b", "c", "d"]);
+			assert.isUndefined(A.baz[0].x);
+		});
+	});
+
+	describe('#copy', function () {
+		it('should copy a basic value', function () {
+			var from = c('/foo'),
+					to = c('/foo2');
+			patcher.copy(from, to);
+			assert.equal(A.foo, A.foo2);
+		});
+
+		it('should copy deeply nested properties', function () {
+			var from = c('/who/what/where'),
+					to = c('/here');
+			patcher.copy(from, to);
+			assert.equal(A.who.what.where, A.here);
+		});
+
+		it('should copy slices to a property', function () {
+			var from = c('/bar/0:2'),
+					to = c('/poo');
+			patcher.copy(from, to);
+			assert.typeOf(A.poo, "array");
+			assert.deepEqual(A.poo, [1, 2]);
+			assert.lengthOf(A.bar, 4);
+		});
+
+		it('should copy an array into a slice', function () {
+			var from = c('/bar'),
+					to = c('/baz/0:1');
+			patcher.copy(from, to);
+			assert.lengthOf(A.baz, 7);
+		});
+
+		it('should copy a slice into an array index', function () {
+			var from = c('/bar/0:3'),
+					to = c('/baz/0');
+			patcher.copy(from, to);
+			assert.lengthOf(A.baz, 5);
+		});
+
+		it('should copy a slice into a slice', function () {
+			var from = c('/bar/1:3'),
+					to = c('/baz/0:1)');
+			patcher.copy(from, to);
+			assert.lengthOf(A.baz, 5);
+			assert.deepEqual(A.baz.slice(0, 2), [2, 3]);
+		});
 	});
 });
